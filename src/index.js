8b28e5679ee8e230cdf5036ab41111e774dbe71e
ruben <davidruben14@gmail.com>
2023-02-23 21:45:35 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./authContext/AuthProvider";
 import HomeRapports from './homeRapport/HomeRapports';
 import MonRapport from './monRapport/MonRapport';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
- <AuthProvider>
- <Routes>
- <Route path="/" element={<App/>} />
- <Route path="mes-rapports" element={<HomeRapports/>}/>
- <Route path='mes-rapports/mon-rapport' element={<MonRapport/>}/>
- </Routes>
- </AuthProvider>
- </BrowserRouter>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+  <BrowserRouter>
+<AuthProvider>
+<Routes>
+<Route path="/" element={<App/>} />
+<Route path="mes-rapports" element={<HomeRapports/>}/>
+<Route path='mes-rapports/mon-rapport' element={<MonRapport/>}/>
+</Routes>
+</AuthProvider>
+</BrowserRouter>
+</React.StrictMode>,
+  document.getElementById('root')
+);
