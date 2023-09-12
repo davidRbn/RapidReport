4f534d49bc5e2fe5f788c916b16ca6b181a59829
ruben <davidruben14@gmail.com>
2023-09-12 13:51:10 +0200

    mise a jour

--- src/homeRapport/HomeRapports.jsx
+++ src/homeRapport/HomeRapports.jsx
@@ -147,6 +147,64 @@ const deleteReport = async (e,idDoc,refRapport,dataInterDelete) => {
 // console.log(rapports.map(data => data.dataInter.filter(dataInter => dataInter.section === 'vueGlobale').map(img => img.image.length > 0 ? console.log(img.image[0].url) :console.log(''))))
 
 
+const getDate = (date) => {
+
+    const theDate = new Date(date)
+
+    console.log(theDate);
+
+    const dateYear = theDate.getFullYear()
+    const dateDay = theDate.getDate()
+    const dateMonth = theDate.getMonth()
+
+    let month = ''
+
+    switch (dateMonth) {
+        case 0:
+          month = 'Janvier';
+          break;
+          case 1:
+            month = 'Février';
+            break;
+            case 2:
+                month = 'Mars';
+                break;
+                case 3:
+                    month = 'Avril';
+                    break;
+                    case 4:
+                        month = 'Mai';
+                        break;
+                        case 5:
+                            month = 'Juin';
+                            break;
+                            case 6:
+                                month = 'Juillet';
+                                break;
+                                case 7:
+                                    month = 'Aout';
+                                    break;
+                                    case 8:
+                                        month = 'Septembre';
+                                        break;
+                                        case 9:
+                                            month = 'Octobre';
+                                            break;
+                                            case 10:
+                                                month = 'Novembre';
+                                                break;
+                                                case 11:
+                                                    month = 'Décembre';
+                                                    break;
+                                                    default :
+                                                    month = ''
+    }
+
+    let dateNew = `${dateDay} ${month} ${dateYear}`
+// console.log(dateDay,dateMonth,dateYear);
+    return dateNew
+}
+
 
 return (
 
@@ -197,8 +255,9 @@ return (
          
                      <div className="text-infoInter-home"> 
                         
-                    <p>{data.infoInter.informationIntervention.client} {data.infoInter.informationIntervention.reference} {data.infoInter.informationIntervention.dateIntervention}</p>
-
+                    <p>{data.infoInter.informationIntervention.client}</p>
+                    <p> {data.infoInter.informationIntervention.reference} </p>
+                    <p>{getDate(data.infoInter.informationIntervention.dateIntervention)}</p>
                     </div>
 
                    
